ece2a77477e2253f560040d7394ad76e552eab0e
darkliang <11710911@mail.sustech.edu.cn>
2019-12-01 17:03:23 +0800

    update: problem detail

--- src/pages/index/views/problem/ProblemDetail.vue
+++ src/pages/index/views/problem/ProblemDetail.vue
@@ -1,5 +1,31 @@
 <template>
-
+    <el-container>
+        <el-header>Header</el-header>
+        <el-main>
+            <el-card class="box-card" shadow="hover" >
+                <div slot="header" class="clearfix">
+                    <span>题目描述</span>
+                    <el-input
+                            type="textarea"
+                            :rows="2"
+                            disabled
+                            v-model="problemDetail.input_description">
+                    </el-input>
+                </div>
+            </el-card>
+            <el-card class="box-card" shadow="hover" >
+                <div class="clearfix">
+                    <span>输入描述</span>
+                </div>
+                <div class="clearfix">
+                    <span>输出描述</span>
+                </div>
+                <div v-for="(sample, index) in problemDetail.samples" :key="index" class="text item">
+                    {{index+1}}
+                </div>
+            </el-card>
+        </el-main>
+    </el-container>
 </template>
 
 <script>
@@ -7,7 +33,7 @@ export default {
   name: 'ProblemDetail',
   data () {
     return {
-      problemDetail: {}
+      problemDetail: null
     }
   },
   methods: {
@@ -30,5 +56,25 @@ export default {
 </script>
 
 <style scoped>
+    .text {
+        font-size: 14px;
+    }
 
+    .item {
+        margin-bottom: 18px;
+    }
+    .clearfix:before,
+
+    .clearfix:after {
+        display: table;
+        content: "";
+    }
+    .clearfix:after {
+        clear: both
+    }
+
+    .box-card {
+        width: 480px;
+        border-radius: 4px;
+    }
 </style>
